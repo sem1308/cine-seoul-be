394c35f04e47cb1bd771aa18afe152801fa8d38f
sem1308 <43841214+sem1308@users.noreply.github.com>
2023-05-19 15:41:35 +0900

    initial commit

---
+++ src/main/java/uos/cineseoul/CineSeoulApplication.java
@@ -0,0 +1,12 @@
+package uos.cineseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineSeoulApplication.class, args);
+	}
+}
---
+++ src/test/java/uos/cineseoul/CineSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.cineseoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
