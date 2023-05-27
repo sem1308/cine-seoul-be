2205da366536ed290defa638918224ffddc4a7df
sem1308 <43841214+sem1308@users.noreply.github.com>
2023-05-27 19:49:29 +0900

    update: add functions about movie

--- src/main/java/uos/cineseoul/controller/ScheduleController.java
+++ src/main/java/uos/cineseoul/controller/ScheduleController.java
@@ -1,8 +1,11 @@
 package uos.cineseoul.controller;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +16,10 @@ import uos.cineseoul.service.ScheduleService;
 import uos.cineseoul.utils.ReturnMessage;
 import uos.cineseoul.utils.enums.StatusEnum;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 @RestController()
@@ -46,10 +52,29 @@ public class ScheduleController {
     @GetMapping("/date/{schedTime}")
     @ResponseStatus(value = HttpStatus.OK)
     @ApiOperation(value = "특정 날짜의 상영일정 조회", protocols = "http")
-    public ResponseEntity<List<PrintScheduleDTO>> lookUpScheduleListByDate(LocalDateTime schedTime) {
-        List<PrintScheduleDTO> scheduleList = scheduleService.findByDate(schedTime);
+    public ResponseEntity<List<PrintScheduleDTO>> lookUpScheduleListByDate(@ApiParam(value = "yyyy-MM-dd", required = true) @PathVariable("schedTime") String schedTime) {
+        LocalDateTime st = LocalDateTime.of(LocalDate.parse(schedTime), LocalTime.of(0,0,0));
+        List<PrintScheduleDTO> scheduleList = scheduleService.findByDate(st);
         return new ResponseEntity<>(scheduleList, HttpStatus.OK);
     }
+
+//    @GetMapping("/movie/{movieNum}")
+//    @ResponseStatus(value = HttpStatus.OK)
+//    @ApiOperation(value = "특정 영화의 상영일정 조회", protocols = "http")
+//    public ResponseEntity<List<PrintScheduleDTO>> lookUpScheduleListByMovie(@PathVariable("movieNum") Long movieNum) {
+//        List<PrintScheduleDTO> scheduleList = scheduleService.findByMovie(movieNum);
+//        return new ResponseEntity<>(scheduleList, HttpStatus.OK);
+//    }
+
+//    @GetMapping("/movie")
+//    @ResponseStatus(value = HttpStatus.OK)
+//    @ApiOperation(value = "특정 영화와 특정 날짜의 상영일정 조회", protocols = "http")
+//    public ResponseEntity<List<PrintScheduleDTO>> lookUpScheduleListByMovieAndDate(@RequestParam(value="movieNum", required = true) Long movieNum,
+//                                                                                  @ApiParam(value = "yyyy-MM-dd", required = true) @RequestParam(value="schedTime", required = true) String schedTime) {
+//        List<PrintScheduleDTO> scheduleList = scheduleService.findByMovie(movieNum);
+//        return new ResponseEntity<>(scheduleList, HttpStatus.OK);
+//    }
+
 
     @PostMapping()
     @ResponseStatus(value = HttpStatus.OK)
