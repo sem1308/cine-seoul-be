e54d783796a5afbcd38b8251bfd2c414dfd0b54d
sem1308 <hshhan1009@naver.com>
2023-05-30 23:36:13 +0900

    Update ScheduleRepository.java

--- src/main/java/uos/cineseoul/repository/ScheduleRepository.java
+++ src/main/java/uos/cineseoul/repository/ScheduleRepository.java
@@ -33,18 +33,18 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     /* 영화별 상영 일정 조회 */
     // - 영화 번호로 조회
-//    @Query("select s from Schedule s where s.movie.movieNum = :movieNum")
-//    List<Schedule> findByMovieNum(@Param("movieNum") String movieNum);
+    @Query("select s from SCHEDULE s where s.movie.movieNum = :movieNum")
+    List<Schedule> findByMovieNum(@Param("movieNum") Long movieNum);
 
     // - 영화 제목으로 조회
-//    @Query("select s from Schedule s where s.movie.movieName = :movieName")
-//    List<Schedule> findByMovieName(@Param("movieName") String movieName);
+    @Query("select s from SCHEDULE s where s.movie.title = :title")
+    List<Schedule> findByMovieTitle(@Param("title") String title);
 
     // 영화(+날짜)별 상영 일정 조회
-    // @Query("select s from Schedule s where s.movie.movieName = :movieName and s.schedTime between :start and :end")
-//    List<Schedule> findByMovieNameAndDateBetween(LocalDateTime start, LocalDateTime end, @Param("movieName") String movieName);
+     @Query("select s from SCHEDULE s where s.movie.title = :title and s.schedTime between :start and :end")
+    List<Schedule> findByMovieNameAndDateBetween(LocalDateTime start, LocalDateTime end, @Param("title") String title);
 
-    // @Query("select s from Schedule s where s.movie.movieNum = :movieNum and s.schedTime between :start and :end")
-//    List<Schedule> findByMovieNumAndDateBetween(LocalDateTime start, LocalDateTime end, @Param("movieNum") String movieNum);
+     @Query("select s from SCHEDULE s where s.movie.movieNum = :movieNum and s.schedTime between :start and :end")
+    List<Schedule> findByMovieNumAndDateBetween(LocalDateTime start, LocalDateTime end, @Param("movieNum") Long movieNum);
 
 }
