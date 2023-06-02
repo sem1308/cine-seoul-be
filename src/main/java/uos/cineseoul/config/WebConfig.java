dd0189fb5abf9a522fc83b275ec6734fd9bf9d55
sem1308 <43841214+sem1308@users.noreply.github.com>
2023-06-03 02:29:45 +0900

    create: cors 해결 config 생성

---
+++ src/main/java/uos/cineseoul/config/WebConfig.java
@@ -0,0 +1,16 @@
+package uos.cineseoul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
+    }
+}
