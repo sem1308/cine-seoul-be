83903542b3c439e3fa3381434a7f0a0d60f1b66a
sem1308 <43841214+sem1308@users.noreply.github.com>
2023-06-08 03:45:32 +0900

    refactor: 필요없는 import 삭제

--- src/main/java/uos/cineseoul/config/SwaggerConfig.java
+++ src/main/java/uos/cineseoul/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package uos.cineseoul.config;
 
-import org.springdoc.core.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestHeader;
