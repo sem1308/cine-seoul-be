fcab64072c8c0254974e6d0941b9a969f87be06b
sem1308 <hshhan1009@naver.com>
2023-06-10 01:00:36 +0900

    update: 좌석 삭제 기능 추가

--- src/main/java/uos/cineseoul/service/SeatService.java
+++ src/main/java/uos/cineseoul/service/SeatService.java
@@ -1,7 +1,6 @@
 package uos.cineseoul.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,9 +15,8 @@ import uos.cineseoul.entity.Screen;
 import uos.cineseoul.entity.Seat;
 import uos.cineseoul.exception.ResourceNotFoundException;
 import uos.cineseoul.mapper.SeatMapper;
-import uos.cineseoul.repository.ScheduleRepository;
-import uos.cineseoul.repository.ScreenRepository;
-import uos.cineseoul.repository.SeatRepository;
+import uos.cineseoul.repository.*;
+import uos.cineseoul.utils.enums.Is;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +27,8 @@ public class SeatService {
     private final SeatRepository seatRepo;
     private final ScreenRepository screenRepo;
     private final ScheduleRepository scheduleRepo;
+    private final ScheduleSeatRepository scheduleSeatRepo;
+    private final TicketSeatRepository ticketSeatRepo;
 
     public List<Seat> findAll() {
         List<Seat> seatList = seatRepo.findAll();
@@ -61,6 +61,13 @@ public class SeatService {
         return seat;
     }
 
+    public Seat findOneByScreenRowCol(Long screenNum, String row, String col) {
+        Seat seat = seatRepo.findByScreenNumAndRowAndCol(screenNum, row ,col).orElseThrow(()->{
+            throw new ResourceNotFoundException("좌석이 존재하지 않습니다.");
+        });
+        return seat;
+    }
+
     public void checkDuplicate(Long screenNum, String row, String col){
         if (seatRepo.findByScreenNumAndRowAndCol(screenNum, row ,col).isPresent()) {
             throw new DuplicateKeyException("좌석이 존재합니다.");
@@ -69,7 +76,19 @@ public class SeatService {
 
     public void checkSchedule(Long screenNum){
         if (!scheduleRepo.findByScreenNum(screenNum).isEmpty()) {
-            throw new DuplicateKeyException("상영일정이 존재하는 상영관에는 좌석을 등록할 수 없습니다.");
+            throw new DuplicateKeyException("상영일정이 존재하는 상영관에는 좌석을 등록 할 수 없습니다.");
+        }
+    }
+
+    public void checkScheduleSeat(Seat seat){
+        if (!scheduleSeatRepo.findAllBySeatAndIsOccupied(seat, Is.Y).isEmpty()) {
+            throw new DuplicateKeyException("상영일정-좌석이 존재하므로 삭제할 수 없습니다.");
+        }
+    }
+
+    public void checkTicketSeat(Seat seat){
+        if (!ticketSeatRepo.findBySeat(seat).isEmpty()) {
+            throw new DuplicateKeyException("티켓-좌석이 존재하므로 삭제할 수 없습니다.");
         }
     }
 
@@ -79,8 +98,6 @@ public class SeatService {
         Screen screen = seatDTO.getScreen();
         // 좌석 중복 체크
         checkDuplicate(screen.getScreenNum(), seatDTO.getRow(), seatDTO.getCol());
-        // 상영일정 확인
-        checkSchedule(screen.getScreenNum());
 
         // 스크린의 총 좌석 값 1 증가
         screen.setTotalSeat(screen.getTotalSeat() + 1);
@@ -93,6 +110,34 @@ public class SeatService {
         Seat newSeat = seatRepo.save(seat);
 
         return newSeat;
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public void delete(Seat seat) {
+        Screen screen = seat.getScreen();
+
+        // 상영일정-좌석 확인
+        checkScheduleSeat(seat);
+        // 티켓-좌석 확인
+        checkTicketSeat(seat);
+
+        // 스크린의 총 좌석 값 1 감소
+        screen.setTotalSeat(screen.getTotalSeat() - 1);
+        screenRepo.save(screen);
+
+        seatRepo.delete(seat);
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public void deleteByNum(Long seatNum) {
+        Seat seat = findOneByNum(seatNum);
+        delete(seat);
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public void deleteByScreenRowCol(Long ScreenNum, String row, String col) {
+        Seat seat = findOneByScreenRowCol(ScreenNum,row,col);
+        delete(seat);
     }
 
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
