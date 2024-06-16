d246ff9a588a9dac9054733c44a0f78f0dcf6a4b
한수한 <hshhan1009@naver.com>
2024-06-16 20:08:45 +0900

    fix : 상영일정_좌석 구조 변경에 따른 코드 변경

--- src/main/java/uos/cineseoul/repository/ScheduleSeatRepository.java
+++ src/main/java/uos/cineseoul/repository/ScheduleSeatRepository.java
@@ -1,6 +1,7 @@
 package uos.cineseoul.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import uos.cineseoul.entity.Schedule;
@@ -10,14 +11,14 @@ import uos.cineseoul.entity.Seat;
 import uos.cineseoul.entity.movie.Movie;
 import uos.cineseoul.utils.enums.Is;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
-public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, ScheduleSeatId> {
+public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Long> {
     // 상영 일정으로 조회
     List<ScheduleSeat> findAllBySchedule(Schedule schedule);
     List<ScheduleSeat> findAllBySeat(Seat seat);
-    List<ScheduleSeat> findAllBySeatAndIsOccupied(Seat seat, Is isOccupied);
 
     // 상영 일정 번호로 조회
     @Query("select ss from SCHEDULE_SEAT ss where ss.schedule.schedNum = :schedNum")
@@ -33,4 +34,12 @@ public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Sche
 
     @Query("select ss from SCHEDULE_SEAT ss where ss.schedule.schedNum = :schedNum and ss.seat.seatNum = :seatNum")
     Optional<ScheduleSeat> findBySchedNumAndSeatNum(@Param("schedNum")Long schedNum ,@Param("seatNum")Long seatNum);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ss from SCHEDULE_SEAT ss where ss.schedule.schedNum = :schedNum and ss.seat.seatNum = :seatNum")
+    Optional<ScheduleSeat> findBySchedNumAndSeatNumForUpdate(@Param("schedNum")Long schedNum ,@Param("seatNum")Long seatNum);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select ss from SCHEDULE_SEAT ss where ss.scheduleSeatNum = :scheduleSeatNum")
+    Optional<ScheduleSeat> findByIdForUpdate(@Param("scheduleSeatNum")Long scheduleSeatNum);
 }
